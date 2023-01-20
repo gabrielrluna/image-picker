9d537b322ee17a52fc437aaab9ec00043392705a
Gabriel Luna <lunagabri@gmail.com>
2023-01-20 11:30:56 -0300

    a

--- App.js
+++ App.js
@@ -3,39 +3,25 @@ import { Button, Image, View } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 
 export default function ImagePickerExample() {
-  const [image, setImage] = useState(null);
+  const [foto, setFoto] = useState();
 
-  const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
+  const acessarCamera = async () => {
+    const imagem = await ImagePicker.launchCameraAsync({
       allowsEditing: true,
-      aspect: [9, 16],
+      aspect: [16, 9],
       quality: 0.5,
     });
 
-    console.log(result);
-
-    if (!result.canceled) {
-      setImage(result.assets[0].uri);
-    }
+    console.log(imagem);
+    setFoto(imagem.assets[0].uri);
   };
-
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button title="Pick an image from camera roll" onPress={pickImage} />
-      {image && (
-        <Image source={{ uri: image }} style={{ width: 300, height: 200 }} />
+      <Button title="Acessar câmera" onPress={acessarCamera} />
+      <Image source={{ uri: foto }} style={{ width: 300, height: 200 }} />
+      {foto && (
+        <Image source={{ uri: foto }} style={{ width: 300, height: 200 }} />
       )}
     </View>
   );
 }
-
-// const estilos = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
